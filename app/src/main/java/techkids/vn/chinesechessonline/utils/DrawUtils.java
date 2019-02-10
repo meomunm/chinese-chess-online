c87158a082be5cfdb3fcf77c47268da2e2053926
MeoMunm <giahungtran1996@gmail.com>
2019-02-10 11:22:01 +0700

    Fixed bug screen.

--- app/src/main/java/techkids/vn/chinesechessonline/utils/DrawUtils.java
+++ app/src/main/java/techkids/vn/chinesechessonline/utils/DrawUtils.java
@@ -10,13 +10,16 @@ import android.util.Log;
 public class DrawUtils {
     private final String TAG = DrawUtils.class.toString();
     static final int LINES = 10, COLUMNS = 9;
+    private final int DIMENSION_HORIZONTAL = 8;
+    private final int DIMENSION_VERTICAL = 9;
 
-    Paint paint;
-    int screenWidth;
-    int screenHeight;
-    Bitmap chessBoard;
-    Canvas canvas;
-    int chessSize;
+
+    private Paint paint;
+    private int screenWidth;
+    private int screenHeight;
+    private Bitmap chessBoard;
+    private Canvas canvas;
+    private int chessSize;
 
     public DrawUtils(int screenWidth, int screenHeight) {
         this.screenWidth = screenWidth;
@@ -25,14 +28,13 @@ public class DrawUtils {
         paint = new Paint();
         paint.setStrokeWidth(1);
         paint.setColor(Color.BLACK);
-        chessBoard = Bitmap.createBitmap(screenWidth, screenHeight, Bitmap.Config.ARGB_8888);
-        canvas = new Canvas();
+        chessBoard = Bitmap.createBitmap(this.screenWidth, this.screenHeight, Bitmap.Config.ARGB_8888);
+        canvas = new Canvas(chessBoard);
     }
 
     public Bitmap drawChessBoard() {
-        drawLines();
-        draw2X();
-        drawCross();
+        drawLines();        //draw | -
+        draw2X();             //draw X
         Log.d(TAG, "debug -> drawChessBoard: ");
         return chessBoard;
     }
@@ -44,24 +46,32 @@ public class DrawUtils {
         //draw columns
         for (int i = 0, x = boardX; i < COLUMNS; i++, x += chessSize) {
             if (i == 0 || i == COLUMNS - 1) {
-                int DESY = boardY + 9 * chessSize;
+                //vẽ 2 đường dọc, ngoài cùng trái và phải
+                int DESY = boardY + DIMENSION_VERTICAL * chessSize;   //độ dài của dòng kẻ
                 canvas.drawLine(x, boardY, x, DESY, paint);
             } else {
-                int DESY = boardY + 4 * chessSize;
+                //vẽ các đường kẻ dọc với chiều dọc ngắn hơn
+                int DESY = boardY + (DIMENSION_HORIZONTAL / 2) * chessSize;
                 canvas.drawLine(x, boardY, x, DESY, paint);
                 canvas.drawLine(x, DESY + chessSize, x, DESY + 5 * chessSize, paint);
             }
         }
 
-        //drawLine
-        for (int i = 0, y = boardY, DESX = boardX + 8 * chessSize; i < LINES; y += chessSize, i++) {
+        //vẽ dòng kẻ ngang
+        for (int i = 0, y = boardY, DESX = boardX + DIMENSION_HORIZONTAL * chessSize; i < LINES; y += chessSize, i++) {
             canvas.drawLine(boardX, y, DESX, y, paint);
         }
     }
 
     private void draw2X() {
-        int pointxy[] = new int[]{0, 3, 2, 5, 2, 3, 0, 5, 7, 3, 9, 5, 9, 3,
-                7, 5};
+        /*          (x;y)
+            start           end
+            (0;3)          (2;5)    \  /
+            (2;3)          (0;5)     \/
+            (7;3)          (9;5)     /\
+            (9;3)          (7;5)    /  \
+         */
+        int pointxy[] = new int[]{0, 3, 2, 5, 2, 3, 0, 5, 7, 3, 9, 5, 9, 3, 7, 5};
         for (int i = 0; i < pointxy.length; i += 4) {
             Point start = getBoardPoint(pointxy[i], pointxy[i + 1]);
             Point end = getBoardPoint(pointxy[i + 2], pointxy[i + 3]);
@@ -69,37 +79,11 @@ public class DrawUtils {
         }
     }
 
-    private void drawCross() {
-        final int pad = chessSize / 13;
-        final int dashLen = chessSize / 7;
-        int crosses[] = new int[]{2, 1, 2, 7, 3, 0, 3, 2, 3, 4, 3, 6, 3, 8, 6, 0, 6, 2, 6, 4, 6, 6, 6, 8, 7, 1, 7, 7};
-        Point point;
-        for (int i = 0; i < crosses.length; i += 2) {
-            point = getBoardPoint(crosses[i], crosses[i + 1]);
-            if (crosses[i + 1] != 0) {
-                //left up
-                canvas.drawLine(point.x - pad, point.y - pad, point.x - pad, point.y - pad - dashLen, paint);
-                canvas.drawLine(point.x - pad, point.y - pad, point.x - pad - dashLen, point.y - pad, paint);
-                //left down
-                canvas.drawLine(point.x - pad, point.y + pad, point.x - pad, point.y + pad + dashLen, paint);
-                canvas.drawLine(point.x - pad, point.y + pad, point.x - pad - dashLen, point.y + pad, paint);
-            }
-            if (crosses[i + 1] != 8) {
-                //right up
-                canvas.drawLine(point.x + pad, point.y - pad, point.x + pad, point.y - pad - dashLen, paint);
-                canvas.drawLine(point.x + pad, point.y - pad, point.x + pad + dashLen, point.y - pad, paint);
-                //right down
-                canvas.drawLine(point.x + pad, point.y + pad, point.x + pad + dashLen, point.y + pad, paint);
-                canvas.drawLine(point.x + pad, point.y + pad, point.x + pad, point.y + pad + dashLen, paint);
-            }
-        }
-    }
-
     private Point getBoardStartPoint() {
         Point point = new Point();
         int chessSize = getChessSize();
-        point.x = (screenWidth - 8 * chessSize) / 2;
-        point.y = (screenWidth - 9 * chessSize) / 2;
+        point.x = (screenWidth - DIMENSION_HORIZONTAL * chessSize) / 2;
+        point.y = (screenHeight - DIMENSION_VERTICAL * chessSize) / 2;
         return point;
     }
 
--- app/src/main/java/techkids/vn/chinesechessonline/views/GameView.java
+++ app/src/main/java/techkids/vn/chinesechessonline/views/GameView.java
@@ -9,6 +9,7 @@ import android.graphics.Color;
 import android.graphics.Matrix;
 import android.graphics.Paint;
 import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
 
 import techkids.vn.chinesechessonline.R;
@@ -34,12 +35,14 @@ public class GameView extends View {
     private boolean canDraw = false;
     private Paint paint;
 
-    private int START_POINT_X = 13;
-    private int START_POINT_Y = 18;
+    private int START_POINT_X;
+    private int START_POINT_Y;
     private boolean firstchoice = true;
-    private int first_i = -1;
-    private int first_j = -1;
+//    private int first_i = -1;
+//    private int first_j = -1;
 
+
+    private Matrix matrix;
 
     public GameView(Context context, GameController gameController) {
         super(context);
@@ -60,7 +63,8 @@ public class GameView extends View {
         paint.setTextSize(chessEdge / 2);
 
         START_POINT_X = (screenWidth - 9 * chessEdge) / 2;
-        START_POINT_Y = ((screenHeight - 9 * chessEdge) / 2) - chessEdge / 2;
+//        START_POINT_Y = ((screenHeight - 9 * chessEdge) / 2) - chessEdge / 2;
+        START_POINT_Y = (screenHeight / 4) - chessEdge;
 
         //...something
         chessBoard = new DrawUtils(screenWidth, screenHeight).drawChessBoard();
@@ -68,25 +72,22 @@ public class GameView extends View {
 
     @Override
     protected void onDraw(Canvas canvas) {
-        Log.d(TAG, "onDraw: DCM");
         this.setBackgroundResource(R.drawable.yangpi);
         canvas.drawBitmap(chessBoard, 0, 0, paint);
 
         if (canDraw) {
-            int i, j = 0;
             int temp;
-            for (i = 0; i < 10; i++) {
-                for (j = 0; j < 9; j++) {
+            for (int i = 0; i < 10; i++) {      //           y   |
+                for (int j = 0; j < 9; j++) {   //           x   -
                     temp = chessModel.getInitPosition()[i][j];
                     if (temp != 0) {
-                        canvas.drawBitmap(chessImage[temp - 1], j * chessEdge + START_POINT_X, i * chessEdge + START_POINT_Y, paint);
+                        canvas.drawBitmap(chessImage[temp - 1], j * chessEdge, i * chessEdge + START_POINT_Y, paint);
                     }
                 }
-                j = 0;
             }
-
-            if (first_i != -1)
-                canvas.drawBitmap(choosedTarget, first_j * chessEdge + START_POINT_X, first_i * chessEdge + START_POINT_Y, paint);
+//
+//            if (first_i != -1)
+//                canvas.drawBitmap(choosedTarget, first_j * chessEdge, first_i * chessEdge + START_POINT_Y, paint);
         }
         Log.d(TAG, "debug -> onDraw: ");
     }
@@ -97,29 +98,30 @@ public class GameView extends View {
         Log.d(TAG, "debug -> initChess: ");
     }
 
-    public void setFirstGo(boolean first) {
+    public void  setFirstGo(boolean first) {
         renderChess(first);
     }
 
     private void renderChess(boolean isRed) {
-        int size = BitmapFactory.decodeResource(res, R.drawable.bbishop).getHeight();
+        int size = BitmapFactory.decodeResource(res, R.drawable.bbishop).getWidth();
 
-        Matrix matrix = new Matrix();
+        matrix = new Matrix();
         float scale = ((float) chessEdge / (float) size);
         matrix.reset();
-        matrix.postScale(scale, scale);
+        matrix.postScale(scale, scale); //ty le hien thi cac o co
+
 
         chooseTarget = Bitmap.createBitmap(BitmapFactory.decodeResource(res, R.drawable.choice), 0, 0, size, size, matrix, true);
         choosedTarget = Bitmap.createBitmap(BitmapFactory.decodeResource(res, R.drawable.choice2), 0, 0, size, size, matrix, true);
 
 
         //tạo ảnh bitmap dựa trên drawable và gộp 2 mảng chessRed và chessBlack vào làm 1
-        //tạo ảnh bitmap của bên cờ đỏ xong tạo tiếp bên cờ đen
         int[] chessRes = isRed ? chessModel.getRedChessRes() : chessModel.getBlackChessRes();
         for (int i = 0; i < chessRes.length; i++) {
             chessImage[i + 7] = Bitmap.createBitmap(BitmapFactory.decodeResource(res, chessRes[i]), 0, 0, size, size, matrix, true);
         }
 
+        //tạo ảnh bitmap của bên cờ đỏ xong tạo tiếp bên cờ đen
         chessRes = !isRed ? chessModel.getRedChessRes() : chessModel.getBlackChessRes();
         matrix.postRotate(180);
         for (int i = 0; i < chessRes.length; i++) {
@@ -136,5 +138,17 @@ public class GameView extends View {
         canDraw = true;
         postInvalidate();
         Log.d(TAG, "renderChess: ");
+    }
+
+
+    //xử lý sự kiện chạm vào quân cờ
+    //handle event when touch chessman
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        if (event.getAction() == MotionEvent.ACTION_DOWN){
+
+            Log.d(TAG, "onTouchEvent: clicked clicked");
+        }
+        return true;
     }
 }
